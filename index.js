45a885b99731f5bac2d7adf1dd27d15e0d2e71af
Siddharth <sahusidd715@gmail.com>
2024-07-31 16:26:28 +0530

    new file:   MOCK_DATA.json 	modified:   index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express=require("express")
 const app=express()
+const users=require("./MOCK_DATA.json")
 const port=8000;
 app.listen(port,()=>{
     console.log(` The server is listening at : ${port} `)
