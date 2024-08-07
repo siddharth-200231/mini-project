6f0cf924b546a2e571640b003696301539b507ca
Siddharth <sahusidd715@gmail.com>
2024-08-07 07:18:59 +0530

    modified:   index.js modified:   package-lock.json modified:   package.json new file:   public/stylesheets/style.css new file:   script.js new file:   tailwind.config.js new file:   views/index.ejs

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express=require("express")
 const app=express()
-const users=require("./MOCK_DATA.json")
-const port=8000;
-app.listen(port,()=>{
-    console.log(` The server is listening at : ${port} `)
-})
+const path=require("path")
+app.set('view engine','ejs')
+app.use(express.static(path.join(__dirname,"public")))
+app.use(express.json())
+app.use(express.urlencoded({extended:true }))
---
+++ script.js
@@ -0,0 +1,2 @@
+let m=require("./index")
+console.log(m.sub(5,2))
