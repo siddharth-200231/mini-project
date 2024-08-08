18c5ad288d245d5051cf040762bdb3d0f9b989b5
Siddharth <sahusidd715@gmail.com>
2024-08-08 10:21:58 +0530

    modified:   index.js 	modified:   models/user.js 	modified:   public/stylesheets/output.css 	modified:   views/index.ejs 	modified:   views/read.ejs

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
-const express=require("express")
-const app=express()
-const path=require("path")
-app.set('view engine','ejs')
-app.use(express.static(path.join(__dirname,"public")))
-app.use(express.json())
-app.use(express.urlencoded({extended:true }))
-app.get('/',(req,res)=>{
-    res.render("index")
-})
-app.get("/read",(req,res)=>{
-    res.render("read")
+const express = require("express");
+const app = express();
+const path = require("path");
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+const userModel = require("./models/user");
+app.get("/", async(req, res) => {
+res.render("index");
+// let allusers= await userModel.find()
+// res.render("read",{allusers})
+});
 
-})
-app.post("/create",(req,res)=>{
-    
-})
-app.listen(8000)
+app.post("/create", async (req, res) => {
+  let Createduser = await userModel.create({
+    username: req.body.name,
+    email: req.body.email,
+    image: req.body.image
+  });
+  res.send(Createduser);
+});
+app.get("/read", (req, res) => {
+  res.render("read");
+});
+app.listen(8000);
--- models/user.js
+++ models/user.js
@@ -1,8 +1,17 @@
-const mongoose=require("mongoose")
-mongoose.connect("mongodb://localhost:27017/users")
-const userschema=mongoose.Schema({
-    username:String,
-    email:String,
-    image:String
-})
-module.exports=mongoose.model("user",userschema)
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost:27017/users", {
+  
+}).then(() => {
+    console.log("Connected to MongoDB");
+}).catch((error) => {
+    console.error("Connection error", error);
+});
+
+const userSchema = new mongoose.Schema({
+    username: String,
+    email: String,
+    image: String
+});
+
+module.exports = mongoose.model("User", userSchema);
